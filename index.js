9d03d623f6c21cf62aa63f2ae9f56187e89b2d4e
Sherry Lin <sherrylin222@gmail.com>
2018-05-13 23:29:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
 }
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
