dc75227ff7caec0d1840f9051e6c3dd66fc7824c
Sherry Lin <sherrylin222@gmail.com>
2018-05-13 23:28:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
-  kittens.push("Ralph");
+  kittens.push(name);
   return kittens;
 }
